cd3866f6086324d4f2b5acaccf3724f42dd3e6b1
Shruti Asolkar <123906416+shrutia003@users.noreply.github.com>
2025-04-18 04:10:39 -0400

    Update sankey.py

--- dashboard/sankey.py
+++ dashboard/sankey.py
@@ -4,7 +4,7 @@ from dash import Dash, dcc, html, Input, Output, State
 import dash_bootstrap_components as dbc
 
 sankey_df = pd.read_csv("Preprocessed_Sankey_Data.csv")
-merged_df = pd.read_csv("Merged_Reviews_With_Features.csv", parse_dates=["Date", "Release Date"])
+merged_df = pd.read_csv("Merged_Reviews_With_Features.csv.gz", compression="gzip", parse_dates=["Date", "Release Date"])
 features_df = pd.read_csv("Features1.csv", encoding='latin1', parse_dates=["Release Date"])
 
 def truncate_label(text, length=40):
